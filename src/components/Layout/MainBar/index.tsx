877a68c537f8d5d487738ae2acf713f7d98b5b4b
madomaga <marcin.domagala@capgemini.com>
2022-11-04 14:54:56 +0100

    Add instagramCard

--- src/components/Layout/MainBar/index.tsx
+++ src/components/Layout/MainBar/index.tsx
@@ -1,7 +1,13 @@
 import {
+  Box,
+  Center,
+  Flex,
   GridItem,
   Stack,
+  Text,
 } from "@chakra-ui/react";
+import Image from "next/image";
+import { BoxWrapper } from "../../Shared/BoxWrapper";
 import SmallBox from "../../Shared/SmallBox";
 
 export default function MainBar({ children }: any) {
@@ -30,16 +36,47 @@ export default function MainBar({ children }: any) {
       textup: '2.348',
       textbottom: 'Weekly Comment',
     },
-
   ]
   return (
     <GridItem>
-      <Stack direction="row" justifyContent={"space-evenly"}>
+      <Stack direction={"row"} justifyContent={"space-between"}>
       {Props?.map((prop, index) => (
         <SmallBox name={prop.name} src={prop.src} textup={prop.textup} textbottom={prop.textbottom} key={index}/>
       ))}
 
       </Stack>
+      <Box>
+        <BoxWrapper>
+          
+            <Flex direction={'column'} alignItems={'center'}>
+            
+              <Box p={'1rem'}>
+                <Center flexDirection={'column'}>
+                <Image src={'https://bit.ly/prosper-baba'} alt={'picture'} width={60} height={60} style={{ borderRadius: '50%' }} />
+                <Text textAlign={'center'}>Lebron James</Text>
+                </Center>
+                </Box>
+              <Box w={'100%'}>
+                <Stack direction={'row'} justifyContent={'space-evenly'} textAlign={'center'}>
+                  <Flex direction={'column'} >
+                    <Text color={'#505050'} fontSize={'12px'} p={0} m={0}>Post</Text>
+                    <Text>498</Text>
+                  </Flex>
+                  <Flex direction={'column'}>
+                    <Text color={'#505050'} fontSize={'12px'} p={0} m={0}>Followers</Text>
+                    <Text>12,459M</Text>
+                  </Flex>
+                  <Flex direction={'column'}>
+                    <Text color={'#505050'} fontSize={'12px'} p={0} m={0}>Following</Text>
+                    <Text>358</Text>
+                  </Flex>
+
+                </Stack>
+              </Box>
+            </Flex>
+            
+        </BoxWrapper>
+        </Box>
     </GridItem>
   );
 }
