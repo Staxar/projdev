f723015cb00f9422cf702af4d877d09e0c5e3bd0
madomaga <marcin.domagala@capgemini.com>
2022-11-04 14:12:55 +0100

    Add top smallbox

--- src/components/Layout/MainBar/index.tsx
+++ src/components/Layout/MainBar/index.tsx
@@ -1,24 +1,44 @@
 import {
-  Avatar,
-  Badge,
-  Box,
-  Center,
-  Flex,
   GridItem,
   Stack,
-  Text,
-  Wrap,
-  WrapItem,
 } from "@chakra-ui/react";
-import { BoxWrapper } from "../../Shared/BoxWrapper";
+import SmallBox from "../../Shared/SmallBox";
+
 export default function MainBar({ children }: any) {
+  const Props = [
+    {
+      src: 'https://bit.ly/dan-abramov',
+      name: 'First image',
+      textup: '2.348',
+      textbottom: 'Weekly Follower',
+    },
+    {
+      src: 'https://bit.ly/ryan-florence',
+      name: 'Second image',
+      textup: '2.348',
+      textbottom: 'Weekly following',
+    },
+    {
+      src: 'https://bit.ly/kent-c-dodds',
+      name: 'Third image',
+      textup: '2.348',
+      textbottom: 'Weekly Like',
+    },
+    {
+      src: 'https://bit.ly/prosper-baba',
+      name: 'Fourth image',
+      textup: '2.348',
+      textbottom: 'Weekly Comment',
+    },
+
+  ]
   return (
     <GridItem>
       <Stack direction="row" justifyContent={"space-evenly"}>
-        <Badge>Default</Badge>
-        <Badge color="green">Success</Badge>
-        <Badge colorScheme="red">Removed</Badge>
-        <Badge colorScheme="purple">New</Badge>
+      {Props?.map((prop, index) => (
+        <SmallBox name={prop.name} src={prop.src} textup={prop.textup} textbottom={prop.textbottom} key={index}/>
+      ))}
+
       </Stack>
     </GridItem>
   );
